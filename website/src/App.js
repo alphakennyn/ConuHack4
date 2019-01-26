d2784875939ecef4a947804e3a50d90d5ab0ee89
Johnny Interiano <benjonant@msn.com>
2019-01-26 15:59:49 -0500

    mo bamba

--- website/src/App.js
+++ website/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import quickstart from 'quickstart.js'
 
 class App extends Component {
   render() {
@@ -19,6 +20,11 @@ class App extends Component {
           >
             Learn React
           </a>
+          <button
+            onClick={quickstart}
+            >
+            Try me
+          </button>
         </header>
       </div>
     );
