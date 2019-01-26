5f81add0ebfc2419f5a4a2e6ce5e4d77bd5c9bca
Kenny Nguyen <kenny>
2019-01-26 16:08:19 -0500

    add new camera feature

--- website/src/CameraComponent.js
+++ website/src/CameraComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Camera from 'react-html5-camera-photo';
+import axios from 'axios';
 import 'react-html5-camera-photo/build/css/index.css';
-
 import './Camera.css';
+import url from './configs/url.json';
 
 class ClCamera extends Component {
   constructor() {
@@ -20,15 +21,28 @@ class ClCamera extends Component {
   }
 
   onTakePhoto(dataUri) {
-    console.log(dataUri);
+    console.log(dataUri.length);
+    axios.post(`${url.endPoint}detectPic`,dataUri).then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
+
+  sendImage(data) {
+    return this.axios.post(`${url}detectPic`,data);
   }
 
   static windowDimension() {
-
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
   }
 
   render() {
     //console.log(windowDim.width);
+    const { width, height} = this.constructor.windowDimension();
     return (
       <div className="app-container">
         <Camera
@@ -36,6 +50,10 @@ class ClCamera extends Component {
             this.onTakePhoto(dataUri); 
           }}
           idealFacingMode="environment"
+          idealResolution={{width, height}}
+          isImageMirror={false}
+          sizeFactor={0.5}
+          //imageCompression={0.8}
         />
       </div>
     )
