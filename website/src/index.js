d7a90b886c907befd2b51d2825ac1373cfdf6a13
Kenny Nguyen <kenny>
2019-01-26 22:41:31 -0500

    oof

--- website/src/index.js
+++ website/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './website/src/index.css';
-import App from './website/src/App.js';
-import * as serviceWorker from './website/src/serviceWorker';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
